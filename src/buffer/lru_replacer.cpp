815a9bded92f89bccd98e1952549d042dafdbebd
xinandeshuo <37105956+xinandeshuo@users.noreply.github.com>
2022-05-08 15:45:30 +0800

    finish lru

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,50 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) {
+  capacity = num_pages;
+}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) { 
+  latch.lock();
+  if (lru_map.empty()) {
+    latch.unlock();
+    return false; 
+  }
+  *frame_id = lru_list.back();
+  lru_list.pop_back();
+  lru_map.erase(*frame_id);
+  latch.unlock();
+  return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  latch.lock();
+  if (lru_map.count(frame_id)) {
+      auto it = lru_map[frame_id];
+      lru_map.erase(frame_id);
+      lru_list.erase(it);
+  }
+  latch.unlock();
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  latch.lock();
+  if(lru_map.count(frame_id)) {
+    latch.unlock();
+    return;
+  }
+  if(lru_map.size() == capacity) {
+    frame_id_t vic = lru_list.back();
+    lru_map.erase(vic);
+  }
+  lru_list.push_front(frame_id);
+  lru_map[frame_id] = lru_list.begin();
+  latch.unlock();
+}
 
-size_t LRUReplacer::Size() { return 0; }
+size_t LRUReplacer::Size() { return lru_map.size(); }
 
 }  // namespace bustub
--- src/include/buffer/lru_replacer.h
+++ src/include/buffer/lru_replacer.h
@@ -15,6 +15,7 @@
 #include <list>
 #include <mutex>  // NOLINT
 #include <vector>
+#include <unordered_map>
 
 #include "buffer/replacer.h"
 #include "common/config.h"
@@ -47,6 +48,10 @@ class LRUReplacer : public Replacer {
 
  private:
   // TODO(student): implement me!
+  uint32_t capacity;
+  std::mutex latch;
+  std::list<frame_id_t> lru_list;
+  std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> lru_map;
 };
 
 }  // namespace bustub
